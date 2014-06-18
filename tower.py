f33cf4ba75e8d643dbe601b87cc263647ba190e7
Svein Ove Undal <sveinou@tihlde.org>
2014-06-18 17:33:48 +0200

    dd

--- tower.py
+++ tower.py
@@ -1,32 +1,51 @@
-#!/usr/bin/env python
-
-import socket, ipaddr
-
+import socket, ipaddr, hashlib, json
+from wall import wall
+from time import strftime
+adminHash = hashlib.sha224("nijet").hexdigest()
+pam = PAM.pam()
 
 class tower():
 
-  def __init__(self,listen,port):
-    try: ver = ipaddr.IPAddress(listen).version
-    except Exception, e: 
-      print e 
-      exit(9) 
-
-    if ver == 4: self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    else: self.sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-    self.sock.bind((listen, port))
-
-
+  def __init__(self,port):
+    self.sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    self.sock.bind(('',port))
+    self.logfile = open('log', 'w')
+  
 
   def listen(self):
-    while True:
+    running = True 
+    while running:
       data, addr = self.sock.recvfrom(1024)
-      print data, addr
+      if self.is_accepted(addr, data):
+        self.command(addr, data.replace('\n',''))
+      else: self.log(addr, data.replace('\n',''), 'No Acces!')
+       
+  def is_accepted(self,stuffs, data):
+    return True 
+
+  def log(self,addr,data,code):
+    out = {'ip': addr[0],'data': data,'code': code, 'when': strftime("%d %b %Y %H:%M:%S")}
+    print out 
+    json.dump(out, self.logfile)
 
 
+  def command(self,addr,data):
+    if len(data.split()) == 3 and wall.get_IPv(data.split()[2]) > 0:
+      if 'add' in data: 
+        wall.add_client(data.split()[2])
+        self.log(addr,data,'Add')
+      if 'del' in data:
+        wall.del_client(data.split()[2])
+	self.log(addr,data,'del')
+
+    if 'rules' in data:
+      self.log(addr,wall.get_rules(int(data.split()[1])),'rules')
+
+  def __del__():
+    self.logfile.close()
+      
 
 
-tower = tower('2001:4662:9ff8:0:1e4b:d6ff:fe88:f5f9',5000)
+tower = tower(5000)
 
 tower.listen()
-	
-    
